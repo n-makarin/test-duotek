86c538ce5fa8ba9ddd91c6b5c4e02706d420e59d
nmakarin <n-makarin@mail.ru>
2019-08-14 18:25:46 +0300

    Add nuxt-link, change linkList prop, add styles
    components\layout\Navigation.vue

--- components/layout/Navigation.vue
+++ components/layout/Navigation.vue
@@ -1,8 +1,18 @@
 <template>
   <div class="navigation">
     <ul>
-      <li v-for="(link, key) in linkList" :key="key">
-        {{ link }}
+      <li
+        v-for="(link, key) in linkList"
+        :key="key"
+        class="navigation__link"
+      >
+        <nuxt-link
+          class="navigation__link-internal"
+          active-class="navigation__link-internal_active"
+          :to="link.path"
+        >
+          {{ link.name }}
+        </nuxt-link>
       </li>
     </ul>
   </div>
@@ -12,10 +22,36 @@
 
 export default {
   data: () => ({
-    linkList: ['проекты', 'задачи', 'новости', 'вакансии', 'контакты']
+    linkList: [
+      { name: 'проекты', path: 'projects' },
+      { name: 'задачи', path: '#' },
+      { name: 'новости', path: '#' },
+      { name: 'вакансии', path: '#' },
+      { name: 'контакты', path: '#' }
+    ]
   })
 }
 </script>
 
 <style lang="scss" scoped>
+.navigation {
+  &__link {
+    display: inline-block;
+    margin-right: 20px;
+    text-transform: capitalize;
+
+    &:last-child {
+      margin-right: 0;
+    }
+
+    &-internal {
+      font-size: 15px;
+      color: black;
+      font-weight: normal;
+      &_active {
+        font-weight: bolder
+      }
+    }
+  }
+}
 </style>
