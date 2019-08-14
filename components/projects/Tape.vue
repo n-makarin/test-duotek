e70f791e86c36b8c63b33997d0d600c3375fc2b7
nmakarin <n-makarin@mail.ru>
2019-08-14 21:22:06 +0300

    Add Tepe.vue markup and props object
    components\projects\Tape.vue

--- components/projects/Tape.vue
+++ components/projects/Tape.vue
@@ -1,12 +1,33 @@
 <template>
   <div class="tape">
-    tape
+    <div
+      v-for="(project, key) in projectList"
+      :key="key"
+      class="tape__project"
+    >
+      <div class="tape__project__title">
+        {{ project.title }}
+      </div>
+      <div class="tape__project__text">
+        {{ project.text }}
+      </div>
+      <div class="tape__project__date">
+        {{ project.date.created }}
+        {{ project.date.edited }}
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
 
 export default {
+  props: {
+    projectList: {
+      type: Array,
+      required: true
+    }
+  }
 }
 
 </script>
