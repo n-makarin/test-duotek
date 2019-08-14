7383be7a4a3f8a5735259b2a5c1e70ea4a5651c3
nmakarin <n-makarin@mail.ru>
2019-08-14 17:40:53 +0300

    Create Header.vue and Footer.vue scaffolds Add them to layouts\default.vue

---
+++ components/layout/Footer.vue
@@ -0,0 +1,14 @@
+<template>
+  <div class="fotoer" />
+</template>
+
+<script>
+
+export default {
+  components: {
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+</style>
--- layouts/default.vue
+++ layouts/default.vue
@@ -1,8 +1,22 @@
 <template>
   <div>
+    <Header />
     <nuxt />
+    <Footer />
   </div>
 </template>
+
+<script>
+import Header from '@/components/layout/Header'
+import Footer from '@/components/layout/Footer'
+
+export default {
+  components: {
+    Header,
+    Footer
+  }
+}
+</script>
 
 <style lang="scss" scoped>
 </style>
