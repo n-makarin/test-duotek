0db50faaf01f6e454ad3fc692c72c87a430c07b7
nmakarin <n-makarin@mail.ru>
2019-08-15 18:17:13 +0300

    Add rowButtons computed prop, add markup
    components\Pagination.vue

--- components/Pagination.vue
+++ components/Pagination.vue
@@ -1,6 +1,20 @@
 <template>
   <div class="pagination">
-    pagination
+    <div class="pagination__row">
+      <div
+        v-for="(button, key) in rowButtons"
+        :key="key"
+        class="pagination-row__item"
+      >
+        <button>{{ button }}</button>
+      </div>
+    </div>
+    <div class="pagination__next-button">
+      <button>Следующая</button>
+    </div>
+    <div class="pagination__last-button">
+      <button>Последняя</button>
+    </div>
   </div>
 </template>
 
@@ -11,6 +25,21 @@ export default {
     selectedPage: {
       type: Number,
       default: 0
+    },
+    pagesAmount: {
+      type: Number,
+      required: true
+    }
+  },
+  computed: {
+    rowButtons () {
+      const buttons = []
+      let count = 0
+      while (count < this.pagesAmount) {
+        buttons.push(count + 1)
+        count++
+      }
+      return buttons
     }
   }
 }
