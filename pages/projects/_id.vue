71c878ffd5102e33462605bfadf3bed55fd4fd27
nmakarin <n-makarin@mail.ru>
2019-08-15 10:48:05 +0300

    Add 'id' computed property
    pages\projects\_id.vue

--- pages/projects/_id.vue
+++ pages/projects/_id.vue
@@ -1,12 +1,17 @@
 <template>
   <div class="page page-projects__id">
-    id
+    {{ id }}
   </div>
 </template>
 
 <script>
 
 export default {
+  computed: {
+    id () {
+      return this.$store.getters['project/id']
+    }
+  }
 }
 </script>
 
