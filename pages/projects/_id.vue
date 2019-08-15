9ba5ddeecc8f1acffbc1c1b3701f543f51d2ad16
nmakarin <n-makarin@mail.ru>
2019-08-15 11:43:06 +0300

    Add hr with styles, rename 'container' to 'main'
    pages\projects\_id.vue

--- pages/projects/_id.vue
+++ pages/projects/_id.vue
@@ -1,13 +1,14 @@
 <template>
-  <div class="page page-projects__id">
-    <div class="projects-id__container">
-      <h1 class="projects-id__container__title">
+  <div class="page page-projects__id projects-id">
+    <div class="projects-id__main">
+      <h1 class="projects-id__main__title">
         {{ project.title }}
       </h1>
-      <div class="projects-id__container__text">
+      <div class="projects-id__main__text">
         {{ project.text }}
       </div>
     </div>
+    <hr>
   </div>
 </template>
 
@@ -25,11 +26,17 @@ export default {
 <style lang="scss" scoped>
 .projects-id {
 
-  &__container {
+  &__main {
 
     &__text {
       line-height: 22px;
     }
+  }
+  hr {
+    height: 1px;
+    margin: 40px 0;
+    border: none;
+    background-color: $light-grey;
   }
 }
 </style>
