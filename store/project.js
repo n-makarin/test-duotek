583b7a9e6edc3f193cdbd514f1a2b68af2890a8b
nmakarin <n-makarin@mail.ru>
2019-08-15 10:43:44 +0300

    Create store\project.js Add state 'id', setId & resetId actions, SET_ID mutation and 'id' getter

---
+++ store/project.js
@@ -0,0 +1,25 @@
+export const state = {
+  id: 0
+}
+
+export const actions = {
+  setId ({ commit }, id) {
+    commit('SET_ID', id)
+  },
+  resetId ({ commit }) {
+    commit('RESET_ID')
+  }
+}
+
+export const mutations = {
+  SET_ID (state, id) {
+    state.id = id
+  },
+  RESET_ID (state) {
+    state.id = 0
+  }
+}
+
+export const getters = {
+  id: state => state.id
+}
