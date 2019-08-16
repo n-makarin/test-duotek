74f5f471eb63e839fa44fdda58a326576acaea21
nmakarin <n-makarin@mail.ru>
2019-08-16 10:56:28 +0300

    Crete local-storage.client.js, add it to plugins
    plugins\vendor\local-storage.client.js

---
+++ plugins/vendor/local-storage.client.js
@@ -0,0 +1,13 @@
+import createPersistedState from 'vuex-persistedstate'
+import * as Cookies from 'js-cookie'
+export default ({ store }) => {
+  createPersistedState({
+    key: 'vuex',
+    paths: [],
+    storage: {
+      getItem: key => Cookies.get(key),
+      setItem: (key, value) => Cookies.set(key, value, { expires: 3, secure: false }),
+      removeItem: key => Cookies.remove(key)
+    }
+  })(store)
+}
