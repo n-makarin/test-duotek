26b2dcb70f7a6fd818ebf6ba719b46916d37dd93
nmakarin <n-makarin@mail.ru>
2019-08-16 12:30:23 +0300

    Add desc. on index page, add padding-bottom
    pages\index.vue

--- pages/index.vue
+++ pages/index.vue
@@ -1,6 +1,12 @@
 <template>
   <div class="page page-index">
-    ahoy
+    <h1>⛵ Ahoy!</h1>
+    <div class="page-index__desctiption">
+      <span>Чтобы продолжить ознакомление с проектом, перейдите во вкладку</span>
+      <nuxt-link to="/projects">
+        проекты
+      </nuxt-link>
+    </div>
   </div>
 </template>
 
@@ -11,4 +17,7 @@ export default {
 </script>
 
 <style lang="scss" scoped>
+.page-index {
+  padding-bottom: 162px;
+}
 </style>
